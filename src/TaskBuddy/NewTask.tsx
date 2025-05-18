a6eaf6a91e3f5146eb796d68779024fdf8aa95da
Richa Singh <richa.singhnxt213@gmail.com>
2025-05-18 18:24:05 +0530

    Adding tasks via forms (temp changes)

--- src/TaskBuddy/NewTask.tsx
+++ src/TaskBuddy/NewTask.tsx
@@ -1,8 +1,42 @@
 import { X } from "lucide-react";
+import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../firebase";
+
 interface Prop {
   onclick: () => void;
 }
 export default function NewTask({ onclick }: Prop) {
+  const [taskList, setTaskList] = useState([]);
+  const [data, setData] = useState({
+    TaskDescription: "",
+    TaskName: "",
+    Date: "",
+  });
+  const handleTaskCreateForm = async (e) => {
+    e.preventDefault();
+    setTaskList((taskList) => [...taskList, data]);
+    const user = auth.currentUser;
+    await setDoc(doc(db, "tasks", user?.uid), {
+      taskList,
+    });
+    setData({ TaskName: "", TaskDescription: "", Date: "" });
+  };
+  let task = "";
+
+  const handleTaskName = (e) => {
+    task = task + e.target.value;
+    setData({ ...data, TaskName: task });
+  };
+
+  const handleTaskDescription = (e) => {
+    setData({ ...data, TaskDescription: e.target.value });
+  };
+
+  const handleDate = (e) => {
+    setData({ ...data, Date: e.target.value });
+  };
+
   return (
     <div
       style={{
@@ -20,16 +54,34 @@ export default function NewTask({ onclick }: Prop) {
         <X onClick={onclick} style={{ cursor: "pointer" }} />
       </p>
       <hr />
-      <form>
-        <input type="text" placeholder="Task title" />
-        <textarea />
+      <form onSubmit={handleTaskCreateForm}>
+        <input
+          type="text"
+          placeholder="Task title"
+          value={data.TaskName}
+          onChange={(e) => handleTaskName(e)}
+        />
+        <textarea
+          value={data.TaskDescription}
+          onChange={(e) => handleTaskDescription(e)}
+        />
         <label>
-          {" "}
-          Due on <input type="date" />
+          Due on
+          <input
+            type="date"
+            value={data.Date}
+            onChange={(e) => handleDate(e)}
+          />
         </label>
         <label>
           <input type="file" placeholder="Drop your files here or update" />
         </label>
+        <div style={{ float: "right" }}>
+          <button>Cancel</button>
+          <input type="submit" placeholder="create" />
+
+          {/* </input> */}
+        </div>
       </form>
     </div>
   );
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCjtB8ECkGTAnOwbquBvGijTPiauxamwm0",
@@ -11,7 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, provider, signInWithPopup };
+export { auth, provider, signInWithPopup, db };
